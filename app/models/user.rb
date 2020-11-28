4bedf8b6b396d75e397382e40d25fe9cc2b3199a
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-11-29 02:24:40 +0900

    サインインとサインアップ機能追加

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+end
