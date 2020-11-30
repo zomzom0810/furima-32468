1196e031511c02c2ce59ef8d7f71d1b437ae1c5f
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-11-30 16:59:51 +0900

    emailに@が含まれていなければ登録できないのテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it "emailに@が含まれていなければ登録できない" do
+        @user.email = "fafafaagmail.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "重複したemailが存在する場合登録できない" do
         @user.save
         another_user = FactoryBot.build(:user)
