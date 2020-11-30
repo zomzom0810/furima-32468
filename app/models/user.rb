2e673fbb5bcd0123d46afd286738d7ec1201cc73
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-11-30 17:10:53 +0900

    バリデーションを「with_options」でまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze #半角英数字混合である
-  validates :nickname, presence: true, length: { maximum: 40}
-  validates :birthday, presence: true
-  validates :first_name_kana, :last_name_kana, presence: true,
-            format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'にはカタカナを使って入力してください'} #カタカナのみ
-  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'}
-  validates :first_name,:last_name, presence: true, 
-  format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には『漢字』、『ひらがな』、『かたかな』を使って入力してください'} #漢字カナかな
+  with_options presence: true do
+    validates :nickname, length: { maximum: 40}
+    validates :birthday
+    validates :first_name_kana, :last_name_kana,
+               format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'にはカタカナを使って入力してください'} #カタカナのみ
+    validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'}
+    validates :first_name,:last_name,
+               format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には『漢字』、『ひらがな』、『かたかな』を使って入力してください'} #漢字カナかな
+end  
 end
