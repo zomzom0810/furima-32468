e1da9ab8949a60fd3faa05565fe2cd0634455608
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-12-02 11:04:23 +0900

    デバッグコード削除

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -3,14 +3,14 @@ window.addEventListener("load",()=>{
     const priceInput = document.getElementById("item-price");
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
-      console.log(inputValue);
+      
   
       const addTaxDom = document.getElementById("add-tax-price")
       addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-       console.log(addTaxDom);
+       
   
       const profitNumber = document.getElementById("profit")
       profitNumber.innerHTML = (Math.floor(inputValue - addTaxDom.innerHTML));
-        console.log(profitNumber);
+        
     })
   })
