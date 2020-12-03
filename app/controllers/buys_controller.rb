f65334e966871054ff0ecb7de4be93847f4877e3
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-12-03 22:46:54 +0900

    buys/indexのviewページ追加と購入ボタンから購入ページに飛ぶ

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -10,10 +10,12 @@ class BuysController < ApplicationController
       redirect_to root_path
     else
       render item_buys_path(buy)
+    end 
   end
 
 
   private
   def buy_params
     params.require(:buy_street).permit(:postal_code, :prefecture, :municipality, :address, :building, :phone_number)
+  end
 end
