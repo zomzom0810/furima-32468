9e1d4718594cd34a2792a49df02c91e5e4ed7f57
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-12-04 00:26:09 +0900

    カード情報の入力失敗したときのrender

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,15 +1,19 @@
 class BuysController < ApplicationController
+
   def index
     @buy_street = BuyStreet.new
+    @item = Item.find(params[:item_id])
+
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @buy_street = BuyStreet.new(buy_params)
     if @buy_street.valid?
       @buy_street.save
       redirect_to root_path
     else
-      render item_buys_path(buy)
+      render :index
     end 
   end
 
