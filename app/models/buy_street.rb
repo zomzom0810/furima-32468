00e5f4a9a9c9d28f098fb8bbe10dd70815b4a568
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-12-06 18:51:39 +0900

    バリデーション追加

--- app/models/buy_street.rb
+++ app/models/buy_street.rb
@@ -9,10 +9,13 @@ class BuyStreet
       validates :address
       validates :phone_number,
                 format: {with: /\A\d{11}\z/, message: "は11桁以内の半角数字で入力してくだい"}
-      validates :token                  
+      validates :token
+      validates :user_id
+      validates :item_id    
+      validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }            
     end
 
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    
 
     def save
       buy = Buy.create(user_id: user_id, item_id: item_id)
